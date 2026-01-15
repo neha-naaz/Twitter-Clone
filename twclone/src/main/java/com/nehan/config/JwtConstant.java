dd417a012bd41365f640e615f3cbc89c19c108f5
neha-naaz <97825754+neha-naaz@users.noreply.github.com>
2026-01-15 13:52:06 +0530

    Delete twclone directory

--- twclone/src/main/java/com/nehan/config/JwtConstant.java
+++
@@ -1,6 +0,0 @@
-package com.nehan.config;
-
-public class JwtConstant {
-    public static String SECRET_KEY = "skjdfhguubjdskshfsoiebjknbjkshdfuorstsrnqruyfh";
-    public static final String JWT_HEADER = "Authorization";
-}
